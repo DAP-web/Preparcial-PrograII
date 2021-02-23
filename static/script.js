9d4ada775354c1419942da2cc37af9e1ed91ee30
Diego Portillo <65975109+DAP-web@users.noreply.github.com>
2021-02-23 16:12:20 -0600

    me siento el mejor

--- static/script.js
+++ static/script.js
@@ -1,4 +1,9 @@
 function processForm(){
+    //document.forms["nombre del formulario"]["id del input"].value
     var name = document.forms["formulario"]["name"].value;
-    alert(name)
+    var age = document.forms["formulario"]["age"].value;
+    var hobby = document.forms["formulario"]["hobby"].value;
+    var mensaje = name + age.toString() + hobby;
+    alert(mensaje);
+    
 }
